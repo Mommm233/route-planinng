149529f861d8d1c34167e54dd6058cc2021ca75f
Mommm233 <88660188+Mommm233@users.noreply.github.com>
2023-06-15 14:56:00 +0800

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,31 +1,29 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import heapq
-import time
 
 
-class IndexNode():
-    def __init__(self, x, y, end, father=None):
+class Node():
+    def __init__(self, x, y, goal, father=None):
         self.x = x
         self.y = y
-        self.h = self.get_estimate(end)
+        self.h = self.get_estimate(goal)
         if father:
             self.update(father)
         else:
             self.g = 0
-            self.f = self.h = self.get_estimate(end)
+            self.f = self.h = self.get_estimate(goal)
             self.father = father
 
     def __lt__(self, a):
         return self.f < a.f
     
-    def get_estimate(self, end):
+    def get_estimate(self, goal):
         '''h(n)'''
-        return np.math.sqrt((self.x - end[0]) ** 2 + (self.y - end[1]) ** 2)
+        return np.math.sqrt((self.x - goal[0]) ** 2 + (self.y - goal[1]) ** 2)
 
-    def get_ground(self, next):
+    def get_ground(self, next_pos):
         '''g(n)'''
-        return 1 if abs(self.x  - next[0]) + abs(self.y - next[1]) == 1 else 1.414
+        return 1 if abs(self.x  - next_pos[0]) + abs(self.y - next_pos[1]) == 1 else 1.414
     
     def update(self, father):
         self.g = father.g + self.get_ground((father.x, father.y))
@@ -34,126 +32,66 @@ class IndexNode():
 
 
 class AStar():
-    def __init__(self, static_map, seed=2):
+    def __init__(self, grid, start, end, seed=2):
         np.random.seed(seed)
-        self.static_map = static_map
-        self.height, self.width = static_map.shape
-
+        self.grid = grid
+        self.height, self.width = grid.shape
         self.directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        
-
-        size = self.height * self.width
-        self.start = np.random.randint(size)
-        self.end = np.random.randint(size)
-        while self.start == self.end:
-            self.end = np.random.randint(size)
-
-        self.path_x = []
-        self.path_y = []
-
-        self.route_length = 0
-        self.search_time = 0
-        self.node_num = 0
-
-    def to2DIndex(self, index):
-        return (index // self.width, index % self.width)
-
-    def create_obstacles(self, rate=0.1):
-        size = self.height * self.width
-        vis = np.zeros(size, dtype='bool')
-        vis[self.start] = vis[self.end] = True
-        obstacles = np.random.choice(
-            [_ for _ in range(size) if not vis[_]],
-            size=int((size - 2) * rate),
-            replace=False
-        )
-
-        x, y = self.to2DIndex(obstacles)
-        self.static_map[x, y] = 0   # color is black
 
     def cross_boundry(self, pos):
         x, y = pos
         return x < 0 or x >= self.height or y < 0 or y >= self.width
     
-    def check_obstacles(self, cur, next):
-        '''安全性的检查障碍物'''
-        if self.static_map[next] == 0: return True
-        if abs(cur[0] - next[0]) + abs(cur[1] - next[1]) != 2:
+    def check_collision(self, cur_pos, next_pos):
+        if self.grid[next_pos] == 0: return True
+        if abs(cur_pos[0] - next_pos[0]) + abs(cur_pos[1] - next_pos[1]) != 2:
             return False
-        # 斜线
-        diagonal1, diagonal2 = (cur[0], next[1]), (next[0], cur[1])
+        diagonal1, diagonal2 = (cur_pos[0], next_pos[1]), (next_pos[0], cur_pos[1])
         if not self.cross_boundry(diagonal1) and not self.cross_boundry(diagonal2):
-            return self.static_map[diagonal1] == 0 and self.static_map[diagonal2] == 0
+            return self.grid[diagonal1] == 0 and self.grid[diagonal2] == 0
         else: return False
 
-    def get_path(self, curNode):
-        self.route_length = round(curNode.g, 3)
+    def restore(self, curNode):
+        path = []
         while curNode:
-            x, y = curNode.x, curNode.y
-            self.path_x.append(x)
-            self.path_y.append(y)
+            path.append([curNode.x, curNode.y])
             curNode = curNode.father
+        return np.array(path)
 
-
-    def plot(self, start, end):
-        fig, ax = plt.subplots()
-        # plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0, hspace=0)
-
-        ax.grid(True, color='black')
-        ax.set_xticks(np.arange(-0.5, self.width, 1), np.arange(0, self.width + 1, 1), alpha=0)   # alpha=0隐藏刻度
-        ax.set_yticks(np.arange(-0.5, self.height, 1), np.arange(self.height, -1, -1), alpha=0)
-
-        ax.text(start[1], start[0], 'S', fontsize=20, fontweight='bold', color='black', ha='center', va='center')
-        ax.text(end[1], end[0], 'E', fontsize=20, fontweight='bold', color='black', ha='center', va='center')
-
-        ax.plot(self.path_y, self.path_x, 'b')
-        ax.imshow(self.static_map, cmap='gray', vmin=0, vmax=255)
-        
-
-    def run(self):
+    def get_path(self, start, end):
         open_list = []
         open_dict, closed_dict = {}, {(-1, -1)}
 
-        start = self.to2DIndex(self.start)
-        end = self.to2DIndex(self.end)
-
-        startNode = IndexNode(start[0], start[1], end)
-
+        startNode = Node(start[0], start[1], end)
         heapq.heapify(open_list) # 堆化
         heapq.heappush(open_list, startNode)
         open_dict.update({start : startNode})
 
-        start_time = time.time()
         while open_list:
             curNode = heapq.heappop(open_list)
-            cur = (curNode.x, curNode.y)
+            cur_pos = (curNode.x, curNode.y)
 
-            if cur == end:
-                self.get_path(curNode)
-                break
-
+            if cur_pos == end:
+                return self.restore(curNode)
+            
             for d in self.directions:
-                next = (cur[0] + d[0], cur[1] + d[1])
-                if next in closed_dict or \
-                    self.cross_boundry(next) or \
-                        self.check_obstacles(cur, next):
+                next_pos = (cur_pos[0] + d[0], cur_pos[1] + d[1])
+                if next_pos in closed_dict or \
+                    self.cross_boundry(next_pos) or \
+                        self.check_collision(cur_pos, next_pos):
                     continue    # 已搜索 越界 障碍 
-                if next in open_dict:
-                    nextNode = open_dict[next]
-                    if nextNode.g > curNode.g + curNode.get_ground(next):
+                if next_pos in open_dict:
+                    nextNode = open_dict[next_pos]
+                    if nextNode.g > curNode.g + curNode.get_ground(next_pos):
                         nextNode.update(curNode)
                         heapq.heapify(open_list)
                 else:
-                    nextNode = IndexNode(next[0], next[1], end, curNode)
+                    nextNode = Node(next_pos[0], next_pos[1], end, curNode)
                     heapq.heappush(open_list, nextNode)
-                    open_dict.update({next : nextNode})
+                    open_dict.update({next_pos : nextNode})
 
-            del open_dict[cur]
-            closed_dict.add(cur)
-        end_time = time.time()
+            del open_dict[cur_pos]
+            closed_dict.add(cur_pos)
 
-        self.search_time = round(end_time - start_time, 3)
-        self.node_num = len(open_list) + len(closed_dict) - 1
-        # self.plot(start, end)
-        return 
+        return np.array([])
     
